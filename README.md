dd9109bb061c690c6412bb9242f3b43b060c2449
Anthony Behnam <anthony.p.behnam@outlook.com>
2024-10-07 02:37:14 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# power_table_calculator.py" 
---
+++ power_table_calculator.py.py
@@ -0,0 +1,9 @@
+import math
+
+print(f'{"a":<5s}{"b":<5s}{"pow(a, b)":<5s}')
+print('-------------------')
+print(f'{7:<5d}{2:<5d}{math.pow(7,2):.1f}')
+print(f'{8:<5d}{3:<5d}{math.pow(8,3):.1f}')
+print(f'{9:<5d}{4:<5d}{math.pow(9,4):.1f}')
+print(f'{10:<5d}{5:<5d}{math.pow(10,5):.1f}')
+print(f'{11:<5d}{6:<5d}{math.pow(11,6):.1f}')
